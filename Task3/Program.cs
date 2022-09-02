e411f54556712dda8d344fd4162c6d771a620423
Listratova Tatyana <listratova1@gmail.com>
2022-09-02 14:10:23 +0300

    Added Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,21 @@
+﻿System.Console.WriteLine("Введите число: ");
+string num = Console.ReadLine();
+try
+{
+    int numConv = Convert.ToInt32(num);
+
+    int result = numConv % 2;
+    if (result == 0)
+    {
+        System.Console.WriteLine("Введенное число - четное");
+    }
+    else
+    {
+        System.Console.WriteLine("Введенное число - нечетное");
+    }
+}
+catch (FormatException)
+{
+    System.Console.WriteLine("Ошибка при конвертации");
+}
+
